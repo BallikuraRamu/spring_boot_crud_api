b4e4936564fa53327d8aec0a1b0e7b3cdbc4756d
BallikuraRamu <ramuballlikura27@gmail.com>
2024-12-19 20:56:33 +0530

    Initial commit

---
+++ src/test/java/com/example/spring_rest_apis/SpringRestApisApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.spring_rest_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.profiles.active=test")
+class SpringRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
